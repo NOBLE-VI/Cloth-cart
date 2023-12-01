65d8796a390c9a49dc9af0f71b26d5889c04a5df
Nikhil-Ghorpade-117 <nikhil_ghorpade@apprely.com>
2023-12-01 20:55:34 +0530

    initial commit - project created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const categories = [
+    { id: 1, title: "Hats" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Mens" },
+    { id: 4, title: "Womens" },
+    { id: 5, title: "Kids" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="categoies-container">
+        {categories.map((category) => {
+          return (
+            <div key={category.id} className="category-container">
+              {/* <img src="" alt="" /> */}
+              <div className="category-body-container">
+                <h2>{category?.title}</h2>
+                <p>Shop now</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
