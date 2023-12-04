d3ff79ac9bdb1a555353614f9ab32bd9148aa063
Nikhil-Ghorpade-117 <nikhil_ghorpade@apprely.com>
2023-12-04 10:30:20 +0530

    added component folder with individual componets

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-function App() {
-  const categories = [
-    { id: 1, title: "Hats" },
-    { id: 2, title: "Jackets" },
-    { id: 3, title: "Mens" },
-    { id: 4, title: "Womens" },
-    { id: 5, title: "Kids" },
-  ];
+import Categories from "./components/categories/categories-main.component.jsx";
 
-  return (
-    <div>
-      <div className="categoies-container">
-        {categories.map((category) => {
-          return (
-            <div key={category.id} className="category-container">
-              {/* <img src="" alt="" /> */}
-              <div className="category-body-container">
-                <h2>{category?.title}</h2>
-                <p>Shop now</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+function App() {
+  return <Categories />;
 }
 
 export default App;
---
+++ src/components/categories/categories-main.component.jsx
@@ -0,0 +1,45 @@
+import "./categories-main.styles.scss";
+import CategoryItemComponent from "../category/category-item.component.jsx";
+
+const Categories = (props) => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return (
+    <div>
+      <div className="categories-container">
+        {categories.map((category) => {
+          return (
+            <CategoryItemComponent key={category.id} category={category} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
